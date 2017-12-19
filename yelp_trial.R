9cf3c9e4c25874bd83733f2208d346787a96ca58
krutim <Kruti@kruti-mehtas-macbook.local>
2017-12-18 22:57:44 -0500

    Conver to tbl

--- yelp_trial.R
+++ yelp_trial.R
@@ -3,11 +3,22 @@
 setwd("/Users/Kruti/Dropbox/Yelp")
 
 #Loads library to read JSON file
-library(rjson)
+library(jsonlite)
+library(tibble)
+library(stringr)
+library(dplyr)
 
 #Loads
-business.data <- fromJSON(file = "/Users/Kruti/Dropbox/DataSets/Yelp/business.json")
+business.data <- stream_in(file("/Users/Kruti/Dropbox/DataSets/Yelp/business.json"))
 data.path <- "/Users/Kruti/Dropbox/DataSets/Yelp"
-user.path <- paste(data.path, "/user.json", sep="")
+user.path <- paste0(data.path, "/user.json")
 
+#Flattening makes the data structure easier to grasp
+business.data.flat <- flatten(business.data)
+
+#Convert to tbl 
+business.data.tbl <- as_data_frame(business.data.flat)
+
+#Filters data by Coffee & Tea category
+business.data.tbl %>% filter(str_detect(categories, "Coffee & Tea"))
 
